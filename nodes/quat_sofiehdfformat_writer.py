e2d3c3472122d5098733e7d91bf114212c79a5fd
¨Adrian <Adrian.Cooke@utwente.nl>
2012-07-05 17:00:38 +0200

    Trying to figure out how to start and stop the node.

--- nodes/quat_sofiehdfformat_writer.py
+++ nodes/quat_sofiehdfformat_writer.py
@@ -1,29 +1,49 @@
 #!/usr/bin/env python
+import sys
 import roslib; roslib.load_manifest('sofiehdfformat_rosdriver')
 import rospy
 from geometry_msgs.msg import QuaternionStamped
 from sofiehdfformat.core.SofieCsvPyTableAccess import SofieCsvPyTableAccess
 
+tableStructure=['quat1','quat2','quat3','quat4','Timestamp']
+def getFileInfo():
+    filename=None
+    runName=None
+    while filename==None or runName == None:
+        try:
+            filename = rospy.get_param('/sofie/filename')
+            runName = rospy.get_param('/sofie/runname')
+        except KeyError:
+            print 'Waiting for filename to be set.'
+    return [filename,runName]
 
-
-def listener():
-    filename = rospy.get_param('/sofie/filename')
-    runName = rospy.get_param('/sofie/runname')
-    rospy.loginfo('Logging to file: '+filename)
-    csvWriter = SofieCsvPyTableAccess(filename,runName,
-        ['quat1','quat2','quat3','quat4','Timestamp'])
-    def callback(data):
-        rospy.loginfo(rospy.get_name()+"Received Quaternion ")
-        rospy.loginfo(data)
+def callback(data):
+        try:
+            shouldWeLog = rospy.get_param('/sofie/shouldWeLog')
+            if shouldWeLog==0:
+                return
+        except KeyError:
+            return
+        rospy.loginfo(rospy.get_name()+": Received Quaternion")
+        rospy.logdebug(data)
         csvWriter.write(
             {'quat1':data.quaternion.x,
             'quat2':data.quaternion.y,
             'quat3':data.quaternion.z,
             'quat4':data.quaternion.w,
             'Timestamp':data.header.stamp.to_time()})
+
+if __name__ == '__main__':
+    argv = rospy.myargv(argv=sys.argv)
+    if len(argv) == 3:
+        filename = argv[1]
+        runName = argv[2]
+        rospy.set_param('/sofie/filename',filename)
+        rospy.set_param('/sofie/runName',runName)
+    else:
+        [filename,runName] = getFileInfo()
+    rospy.loginfo('Logging to file: '+filename+' runName:'+runName)
+    csvWriter = SofieCsvPyTableAccess(filename,runName,tableStructure)
     rospy.init_node('sofiehdfformatwriter', anonymous=True)
     rospy.Subscriber("quat_sofiehdfformat", QuaternionStamped, callback)
     rospy.spin()
-
-if __name__ == '__main__':
-    listener()
