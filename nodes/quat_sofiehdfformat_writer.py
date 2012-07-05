9e098b9544010f3aacf140488e6aa4ef8d1fa2bb
¨Adrian <Adrian.Cooke@utwente.nl>
2012-07-05 12:17:39 +0200

    Basic driver now works for listening on a channel that broadcasts geometry_msgs.msg.QuaternionStamped messages.

--- nodes/quat_sofiehdfformat_writer.py
+++ nodes/quat_sofiehdfformat_writer.py
@@ -1,11 +1,23 @@
 #!/usr/bin/env python
-import roslib; roslib.load_manifest('ros_sofiehdfformat')
+import roslib; roslib.load_manifest('sofiehdfformat_rosdriver')
 import rospy
 from geometry_msgs.msg import QuaternionStamped
-def callback(data):
-    rospy.loginfo(rospy.get_name()+"Received Quaternion %s",data.data)
+from sofiehdfformat.core.SofieCsvPyTableAccess import SofieCsvPyTableAccess
+
+
 
 def listener():
+    csvWriter = SofieCsvPyTableAccess('./test.h5','testrun',
+        ['quat1','quat2','quat3','quat4','Timestamp'])
+    def callback(data):
+        rospy.loginfo(rospy.get_name()+"Received Quaternion ")
+        rospy.loginfo(data)
+        csvWriter.write(
+            {'quat1':data.quaternion.x,
+            'quat2':data.quaternion.y,
+            'quat3':data.quaternion.z,
+            'quat4':data.quaternion.w,
+            'Timestamp':data.header.stamp.to_time()})
     rospy.init_node('sofiehdfformatwriter', anonymous=True)
     rospy.Subscriber("quat_sofiehdfformat", QuaternionStamped, callback)
     rospy.spin()
