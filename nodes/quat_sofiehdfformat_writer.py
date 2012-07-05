bd0878e493fdc64415a998478d5a197aa0998380
¨Adrian <Adrian.Cooke@utwente.nl>
2012-07-05 10:43:34 +0200

    Working on the listerner that will write to HDFFormat.

--- nodes/quat_sofiehdfformat_writer.py
+++ nodes/quat_sofiehdfformat_writer.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
 import roslib; roslib.load_manifest('ros_sofiehdfformat')
 import rospy
-from std_msgs.msg import String
+from geometry_msgs.msg import QuaternionStamped
 def callback(data):
-    rospy.loginfo(rospy.get_name()+"I heard %s",data.data)
+    rospy.loginfo(rospy.get_name()+"Received Quaternion %s",data.data)
 
 def listener():
     rospy.init_node('sofiehdfformatwriter', anonymous=True)
-    rospy.Subscriber("sofiehdfformat", String, callback)
+    rospy.Subscriber("quat_sofiehdfformat", QuaternionStamped, callback)
     rospy.spin()
 
 if __name__ == '__main__':
