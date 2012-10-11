2aa0252a784728d843fe7b0894384a23340e6664
¨Adrian <Adrian.Cooke@utwente.nl>
2012-10-11 15:58:40 +0200

    Added forgotten __init__.py file and changed to importdata sofiehdfformat funciton.

--- sofiehdfformat_rosdriver/src/sofiehdfformat_rosdriver/import_csv_data.py
+++ sofiehdfformat_rosdriver/src/sofiehdfformat_rosdriver/import_csv_data.py
@@ -1,21 +1,17 @@
-from sofiehdfformat.core.SofieCsvPyTableAccess import SofieCsvPyTableAccess
-from sofiehdfformat.core.SofieCsvParser import parse_sample_interpret as csv_sample_interpret
-from sofiehdfformat.core.SofieCsvAccess import SofieCsvAccess
-from sofiehdfformat.core.SofieCsvFile import CsvFile
+from sofiehdfformat.core.SofieFileUtils import importdata
 import sys
 TMPCSVFILE='/tmp/ar-csv-tm.csv'
 tableStructure = ['id', 'confidence', 'x', 'y', 'z', 'quat1', 'quat2', 'quat3', 'quat4', 'timestamp']
 def importARData(filename, runName):
-    print '..importing the AR information'
+    print 'IMPORTING AR DATA'
     try:
-        theReader = CsvFile(TMPCSVFILE)
-        print '.'
-        csvWriter = SofieCsvPyTableAccess(filename, runName, tableStructure)
-        for row in theReader:
-            parsed = csv_sample_interpret(row)
-            csvWriter.write(parsed)
-        csvWriter.close();
-        theReader.close();
+        if os.path.isfile(TMPCSVFILE):
+            importdata(TMPCSVFILE,
+                filename,
+                runName,
+                'description',
+                True,
+                False)
         print 'FINISHED PARSING AR INTO SOFIEHDFFILE.'
     except:
         print "Unexpected error:", sys.exc_info()[0]
