4eccd2360a8a0ce30eab5cb359c15e8796a37fcb
¨Adrian <Adrian.Cooke@utwente.nl>
2012-12-19 17:13:12 +0100

    Working the CLONE install.

--- CLONE_SETUP.sh
+++ CLONE_SETUP.sh
@@ -1,2 +1,6 @@
 cat .gitmodules| sed 's|git@github.com:agcooke/|git://github.com/agcooke/|g' > tmp
 mv tmp .gitmodules
+git submodule init
+git submodule update
+cd sofiehdfformat_rosdriver/
+rosmake
