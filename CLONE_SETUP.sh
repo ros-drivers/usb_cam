b337043be2cddc490d32c18dfbb2e8e16751a55d
¨Adrian <Adrian.Cooke@utwente.nl>
2012-12-20 10:45:37 +0100

    Modified the README and CLONE_SETUP.sh

--- CLONE_SETUP.sh
+++ CLONE_SETUP.sh
@@ -1,6 +1,16 @@
+#Change Git Modules to readonly.
 cat .gitmodules| sed 's|git@github.com:agcooke/|git://github.com/agcooke/|g' > tmp
 mv tmp .gitmodules
+#Install Submodules.
 git submodule init
 git submodule update
-cd sofiehdfformat_rosdriver/
+#Build Ros
+roscd sofiehdfformat_rosdriver/
 rosmake
+#Enter VirtualEnv
+workon sofie
+#Install Python Packages.
+cd ~/ros/libs/sofiehdformat/
+sudo python setup.py develop
+cd ~/ros/libs/experimentcontrol/
+sudo python setup.py develop
