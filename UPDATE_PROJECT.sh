c74714177c527ed2a0638c22b5c8e9fc276cd68f
¨Adrian <Adrian.Cooke@utwente.nl>
2013-02-06 14:01:41 +0100

    Fixed update script.

--- UPDATE_PROJECT.sh
+++ UPDATE_PROJECT.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 git pull origin master
 cat .rosinstall | sed 's|git@github.com:agcooke/|git://github.com/agcooke/|g' > .rosinstall.tmp
+mv .rosinstall.tmp .rosinstall
 rosws update
 catkin_make
 source devel/setup.sh 
 workon sofie
 pip install -e git+git://github.com/agcooke/Sofie-HDF-Format.git#egg=sofiehdfformat --upgrade
-pip install -e git+git://github.com/agcooke/ExperimentControl.git#experimentcontrol --upgrade
+pip install -e git+git://github.com/agcooke/ExperimentControl.git#egg=experimentcontrol --upgrade
