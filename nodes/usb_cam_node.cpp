4bf6409deb505514ec3aaede2b4fbf2b07031e69
David Kent <davidkent@wpi.edu>
2014-10-28 15:41:50 -0400

    Parameter adjusting

--- nodes/usb_cam_node.cpp
+++ nodes/usb_cam_node.cpp
@@ -132,20 +132,6 @@ public:
     camera_image_ = usb_cam_camera_start(video_device_name_.c_str(), io_method, pixel_format, image_width_,
                                          image_height_, framerate_);
 
-    // set camera parameters
-    std::stringstream paramstream;
-    paramstream << "brightness=" << brightness_;
-    this->set_v4l_parameters(video_device_name_, paramstream.str());
-    paramstream.str("");
-    paramstream << "contrast=" << contrast_;
-    this->set_v4l_parameters(video_device_name_, paramstream.str());
-    paramstream.str("");
-    paramstream << "saturation=" << saturation_;
-    this->set_v4l_parameters(video_device_name_, paramstream.str());
-    paramstream.str("");
-    paramstream << "sharpness=" << sharpness_;
-    this->set_v4l_parameters(video_device_name_, paramstream.str());
-
     // check auto focus
     if (autofocus_)
     {
@@ -154,6 +140,7 @@ public:
     }
     else
     {
+      this->set_v4l_parameters(video_device_name_, "focus_auto=0");
       std::stringstream ss;
       ss << "focus_absolute=" << focus_;
       this->set_v4l_parameters(video_device_name_, ss.str());
@@ -168,7 +155,23 @@ public:
       std::stringstream ss;
       ss << "exposure_absolute=" << exposure_;
       this->set_v4l_parameters(video_device_name_, ss.str());
+
+      // set other camera parameters
+      ss.str("");
+      ss << "brightness=" << brightness_;
+      this->set_v4l_parameters(video_device_name_, ss.str());
+      ss.str("");
+      ss << "contrast=" << contrast_;
+      this->set_v4l_parameters(video_device_name_, ss.str());
+      ss.str("");
+      ss << "saturation=" << saturation_;
+      this->set_v4l_parameters(video_device_name_, ss.str());
+
     }
+
+    std::stringstream ss;
+    ss << "sharpness=" << sharpness_;
+    this->set_v4l_parameters(video_device_name_, ss.str());
   }
 
   virtual ~UsbCamNode()
