8b795a4a31ef1e1bdd369e5413a446dede53be27
Sander van Dijk <sgvandijk@gmail.com>
2018-12-02 12:29:51 +0000

    Add test params matching old launch file

--- launch/usb_cam-test.launch.py
+++ launch/usb_cam-test.launch.py
@@ -1,13 +1,14 @@
 import os
 
+import launch
 from launch import LaunchDescription
 from launch_ros.actions import Node
-
+from launch.substitutions import ThisLaunchFileDir
 
 def generate_launch_description():
     return LaunchDescription([
         Node(package='usb_cam', node_executable='usb_cam_node',
-             output='screen'),
+             output='screen', arguments=[['__params:=', ThisLaunchFileDir(), '/test_params.yaml']]),
         Node(package='image_tools', node_executable='showimage',
              output='screen', arguments=['-t', '/image_raw', '-r', '0'])
 ])
