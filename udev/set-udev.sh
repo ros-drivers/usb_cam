2236e968e70372ccf7d139a793cc5be6f759f83f
caps <caps.yang@plus.ai>
2019-08-21 22:03:50 +0800

    change_pixel_and_set_udev

---
+++ udev/set-udev.sh
@@ -0,0 +1,4 @@
+sudo cp usb-cam-0231-1.rules /etc/udev/rules.d/usb-cam-0231-1.rules
+sudo cp usb-cam-0231-2.rules /etc/udev/rules.d/usb-cam-0231-2.rules
+sudo service udev reload
+sudo service udev restart
