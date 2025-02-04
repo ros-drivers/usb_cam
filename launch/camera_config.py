53807a07778754536a1f08769c95865943abf4ea
Colin Moore <camoore7@ncsu.edu>
2025-02-04 13:26:52 -0500

    Update to Pydantic 2.9

--- launch/camera_config.py
+++ launch/camera_config.py
@@ -31,7 +31,7 @@ from pathlib import Path
 from typing import List, Optional
 
 from ament_index_python.packages import get_package_share_directory
-from pydantic import BaseModel, root_validator, validator
+from pydantic import BaseModel, model_validator, field_validator
 
 USB_CAM_DIR = get_package_share_directory('usb_cam')
 
@@ -42,13 +42,13 @@ class CameraConfig(BaseModel):
     remappings: Optional[List]
     namespace: Optional[str]
 
-    @validator('param_path')
+    @field_validator('param_path')
     def validate_param_path(cls, value):
         if value and not value.exists():
             raise FileNotFoundError(f'Could not find parameter file: {value}')
         return value
 
-    @root_validator
+    @model_validator(mode='before')
     def validate_root(cls, values):
         name = values.get('name')
         remappings = values.get('remappings')
