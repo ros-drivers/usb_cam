f7de6d40d363441bf14e96c1c52c05eb48b4634c
Dominykas Strazdas <dominykas.strazdas@ovgu.de>
2025-08-13 15:41:59 +0200

    Update webcam configuration files + reintroduce launch files for rondor and rosa webcams.

---
+++ launch/robot_webcam.launch.py
@@ -0,0 +1,105 @@
+# Copyright 2018 Lucas Walter
+# All rights reserved.
+#
+# Software License Agreement (BSD License 2.0)
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#
+#  * Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+#  * Redistributions in binary form must reproduce the above
+#    copyright notice, this list of conditions and the following
+#    disclaimer in the documentation and/or other materials provided
+#    with the distribution.
+#  * Neither the name of Lucas Walter nor the names of its
+#    contributors may be used to endorse or promote products derived
+#    from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+# COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+import argparse
+import os
+from pathlib import Path  
+import sys
+
+print("Starting Webcam Launch File")
+
+# Hack to get relative import of .camera_config file working
+dir_path = os.path.dirname(os.path.realpath(__file__))
+sys.path.append(dir_path)
+
+from camera_config import CameraConfig
+from launch import LaunchDescription  
+from launch.actions import GroupAction  
+from launch_ros.actions import Node  
+#get this package path
+USB_CAM_DIR = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+print("Finished importing modules")
+
+
+CAMERAS = []
+CAMERAS.append(
+    CameraConfig(
+        name='rgb',
+        namespace='rosa',
+        param_path=Path(USB_CAM_DIR, 'config', 'rosa_webcam.yaml')
+    )
+)
+CAMERAS.append(
+    CameraConfig(
+        name='rgb',
+        namespace='rondor',
+        param_path=Path(USB_CAM_DIR, 'config', 'rondor_webcam.yaml')
+    )
+    # Add more Camera's here and they will automatically be launched below
+)
+
+print("Finished configuring cameras")
+
+def generate_launch_description():
+    ld = LaunchDescription()
+
+    parser = argparse.ArgumentParser(description='usb_cam demo')
+    parser.add_argument('-n', '--node-name', dest='node_name', type=str,
+                        help='name for device', default='usb_cam')
+
+    camera_nodes = [
+        Node(
+            package=__package__ if __package__ else 'usb_cam',
+            executable='usb_cam_node_exe', output='screen',
+            name=camera.name,
+            namespace=camera.namespace,
+            parameters=[camera.param_path],
+            remappings=camera.remappings
+        )
+        for camera in CAMERAS
+    ]
+
+    camera_group = GroupAction(camera_nodes)
+
+    ld.add_action(camera_group)
+    return ld
+
+if __name__ == '__main__':
+    print("Running generate_launch_description()")
+    launch_description = generate_launch_description()
+    print("Launch description generated")
+    
+    # Normally you would use a ROS 2 launch system to run this, but for testing:
+    from launch import LaunchService
+    ls = LaunchService()
+    ls.include_launch_description(launch_description)
+    ls.run()
--- launch/rosa_webcam.launch.py
+++ launch/rosa_webcam.launch.py
@@ -1,3 +1,35 @@
+# Copyright 2018 Lucas Walter
+# All rights reserved.
+#
+# Software License Agreement (BSD License 2.0)
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#
+#  * Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+#  * Redistributions in binary form must reproduce the above
+#    copyright notice, this list of conditions and the following
+#    disclaimer in the documentation and/or other materials provided
+#    with the distribution.
+#  * Neither the name of Lucas Walter nor the names of its
+#    contributors may be used to endorse or promote products derived
+#    from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+# COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
 import argparse
 import os
 from pathlib import Path  
@@ -21,21 +53,11 @@ print("Finished importing modules")
 CAMERAS = []
 CAMERAS.append(
     CameraConfig(
-        name='webcam_node',
+        name='rgb',
         namespace='rosa',
         param_path=Path(USB_CAM_DIR, 'config', 'rosa_webcam.yaml')
     )
 )
-CAMERAS.append(
-    CameraConfig(
-        name='webcam_node',
-        namespace='rondor',
-        param_path=Path(USB_CAM_DIR, 'config', 'rondor_webcam.yaml')
-    )
-    # Add more Camera's here and they will automatically be launched below
-)
-
-print("Finished configuring cameras")
 
 def generate_launch_description():
     ld = LaunchDescription()
